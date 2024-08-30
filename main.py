319978dd9bc352c04a8e6a411842dd460843fca3
gyeonggyeong <ehrehrsnrh@gmail.com>
2024-08-30 17:44:22 +0900

    load_data 인터페이스 추가

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # main.py
-
+# streamlit을 이용한 주가 차트
 import streamlit as st
+from data_loader import load_data
+
 
 def main():
     st.title("this is a dashboard")
-
+    df = load_data()
+    
 if __name__ == '__main__':
     main()
